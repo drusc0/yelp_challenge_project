023763d2ef1005a6a6ff6694a9d1fefdd088a4ea
drusc0 <drusc0@hotmail.com>
2015-11-30 19:13:47 -0500

    removestopwords in search class to avoid using too many clauses in boolean query

--- src/ilz534/Search.java
+++ src/ilz534/Search.java
@@ -8,8 +8,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
@@ -17,31 +18,31 @@ import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
 
 /**
  * Search.java
  * 
- * Performs a search on the index built for the purpose to find similar 
- * labels or categories for the remaining 30% businesses. We use this subset
- * to see how accurate reviews and/or texts can be in labeling a business
- * as some yelp category
+ * Performs a search on the index built for the purpose to find similar labels
+ * or categories for the remaining 30% businesses. We use this subset to see how
+ * accurate reviews and/or texts can be in labeling a business as some yelp
+ * category
  * 
  * @author drusc0
  */
 public class Search {
-	
+
 	private static final String PATH = "/Volumes/SEAGATE1TB/Yelp/index";
 	private Connector con;
 	private IndexReader reader;
 	private IndexSearcher searcher;
 	private Analyzer analyzer;
-	
+
 	/**
-	 * establishes connection with mongo and initialized reader, searcher, and analyer
+	 * establishes connection with mongo and initialized reader, searcher, and
+	 * analyer
+	 * 
 	 * @throws IOException
 	 */
 	public Search() throws IOException {
@@ -49,70 +50,105 @@ public class Search {
 		this.reader = DirectoryReader.open(FSDirectory.open(Paths.get(PATH)));
 		this.searcher = new IndexSearcher(this.reader);
 		this.analyzer = new StandardAnalyzer();
-		//set searcher to best match similarity model
+		// set searcher to best match similarity model
 		this.searcher.setSimilarity(new BM25Similarity());
 	}
-	
+
 	/**
-	 * getTestingSet()
-	 * we retrieve the remaining 30% of businesses to test accuracy of reviews
-	 * for labeling categories for incoming businesses.
+	 * getTestingSet() we retrieve the remaining 30% of businesses to test
+	 * accuracy of reviews for labeling categories for incoming businesses.
+	 * 
 	 * @return testingSet
 	 */
 	public List<org.bson.Document> getTestingSet() {
 		List<org.bson.Document> testingSet = this.con.getBusinessTestingSet();
 		return testingSet;
 	}
-	
+
 	/**
-	 * parseQuery
-	 * Parses the text in each business tip and/or review
+	 * parseQuery Parses the text in each business tip and/or review
+	 * 
 	 * @param queryString
-	 * @param field - TIP|REVIEW
+	 * @param field
+	 *            - TIP|REVIEW
 	 * @return terms
 	 * @throws IOException
 	 * @throws ParseException
 	 */
-	public Set<Term> parseQuery(String queryString, String field) 
+	public Set<Term> parseQuery(String queryString, String field)
 			throws IOException, ParseException {
-		
+
 		Set<Term> terms = new LinkedHashSet<Term>();
 		QueryParser parser = new QueryParser(field, this.analyzer);
 		Query query = parser.parse(queryString);
 		this.searcher.createNormalizedWeight(query, false).extractTerms(terms);
 		return terms;
 	}
-	
-	
-	
+
 	public void rankDoccuments() throws IOException, ParseException {
-		
+
 		QueryParser parser = new QueryParser("REVIEW", this.analyzer);
-		
+
 		List<org.bson.Document> testingSet = this.getTestingSet();
-		//extract the remaining dataset (testing)
-		for(org.bson.Document doc : testingSet) {
-			
+		// extract the remaining dataset (testing)
+		for (org.bson.Document doc : testingSet) {
+
 			String docID = doc.getString("business_id");
-			//get review list based on doc ids
+			// get review list based on doc ids
 			List<org.bson.Document> reviewList = this.getReviewList(docID);
 			String txt = this.getText(reviewList);
-			//parse queries
-			Query query = parser.parse(txt);
-			//get top 1000 results
-			TopDocs results = this.searcher.search(query, 1000);
-			ScoreDoc[] hits = results.scoreDocs;
-			for(int i = 0; i < hits.length; i++) {
-				Document document = this.searcher.doc(hits[i].doc);
-				System.out.println(document.get("DOCNO"));
+
+			try {
+				Query query = parser.parse(removeStopWords(txt));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			// parse queries
+			// Query query = parser.parse(QueryParser.escape(txt));
+
+			// get top 1000 results
+			/*
+			 * TopDocs results = this.searcher.search(query, 1000); ScoreDoc[]
+			 * hits = results.scoreDocs; for(int i = 0; i < hits.length; i++) {
+			 * Document document = this.searcher.doc(hits[i].doc);
+			 * System.out.println(document.get("DOCNO")); }
+			 */
 		}
 	}
+
 	
 	/**
-	 * getReviewList 
-	 * passes the business id to find the reviews for a specific
+	 * removeStopWords
+	 * delete stop words from query string
+	 * 
+	 * @param textFile
+	 * @return
+	 * @throws Exception
+	 */
+	public static String removeStopWords(String textFile) throws Exception {
+		Analyzer analyzer = new StandardAnalyzer();
+		TokenStream ts = analyzer.tokenStream("content", textFile);
+		StringBuilder builder = new StringBuilder();
+		CharTermAttribute charTerm = ts.addAttribute(CharTermAttribute.class);
+
+		try {
+			ts.reset(); // Resets this stream to the beginning. (Required)
+			while (ts.incrementToken()) {
+				builder.append(charTerm.toString() + " ");
+
+			}
+			ts.end(); // Perform end-of-stream operations, e.g. set the final
+						// offset.
+		} finally {
+			ts.close(); // Release resources associated with this stream.
+		}
+		return builder.toString();
+	}
+
+	/**
+	 * getReviewList passes the business id to find the reviews for a specific
 	 * business
+	 * 
 	 * @param businessID
 	 * @return reviewList
 	 */
@@ -122,11 +158,10 @@ public class Search {
 				.into(new ArrayList<org.bson.Document>());
 		return reviewList;
 	}
-	
-	
+
 	/**
-	 * getText 
-	 * concatenates all text from the query list
+	 * getText concatenates all text from the query list
+	 * 
 	 * @params List<org.bson.Document>
 	 * @return String of all text items
 	 */
@@ -143,7 +178,7 @@ public class Search {
 			}
 		}
 
-		return strBuilder.toString().replace("\"", "");
+		return strBuilder.toString();
 	}
 
 }
